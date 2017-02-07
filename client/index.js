7eb09ee72bfc790d746686b0b1c86b60817aef8d
Syanmil <syanmil@yahoo.com>
2017-02-07 21:29:37 +0700

    upgrading hidden element with price and id

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   listAllItem()
 })
-function addToCart(item){
+function addToCart(item, price, id){
   let qty = 1
   if(localStorage.getItem(item)){
     prevQty = localStorage.getItem(item)
@@ -12,12 +12,15 @@ function addToCart(item){
   }
   refreshCart()
 }
-function refreshCart() {
+function objCart() {
   let cart = []
   for (var item in localStorage) {
     cart.push({name: item, qty: localStorage[item]});
   }
-  cart = cart.slice(0, localStorage.length)
+  return cart.slice(0, localStorage.length)
+}
+function refreshCart() {
+  let cart = objCart()
   $('#cartItem').empty()
   cart.forEach(function(item){
     $('#cartItem').append(
@@ -42,6 +45,8 @@ function listAllItem(){
               <p>Stock: ${item.stock}</p>
               <p>Price: ${item.price}</p>
               <button type="button" name="button" class="btn btn-success cartlist" id="${item.name}">Add to Cart</button>
+              <input type="hidden" id="price${item.name}" value="${item.price}">
+              <input type="hidden" id="id${item.name}" value="${item.id}">
             </div>
           </div>`
         )
@@ -54,12 +59,30 @@ function listAllItem(){
     addlistener()
   })
 }
+function checkout() {
+  let cart = objCart()
+  $.ajax({
+    type: 'POST',
+    url: 'http://localhost:3000/api/checkout',
+    data: {cartItem: cart},
+    dataType: 'json',
+    success: function(status) {
+      $('#endCheckout').prepend(`<p>${status}</p>`)
+    },
+    fail: function(err){
+      console.log(err);
+    }
+  })
+}
 function addlistener(){
   $('#itemlist').on('click', '.btn.cartlist', function(){
-    addToCart(this.id)
+    let price = $(`#price${this.id}`).val()
+    let id = $(`#id${this.id}`).val()
+    addToCart(this.id, price, id)
     alert('Added To Cart '+this.id);
   })
   $('#mycart').on('click', '.btn.btn-success.btn-lg.checkout', function(){
+    checkout()
     localStorage.clear();
     alert('Buy Items Success');
   })
