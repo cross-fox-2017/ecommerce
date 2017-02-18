7423aae766662d61c32dcd1ef5c769bb2c7aa306
YOMA <email": null>
2017-02-18 15:01:58 +0700

    membuat route untuk customers

--- server/routes/items.js
+++ server/routes/items.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const items = require('../controllers/items');
 
-router.post('/items',items.addItems);
+router.post('/',items.addItems);
 router.get('/',items.getAllItems);
 router.put('/',items.checkOutItems)
 
